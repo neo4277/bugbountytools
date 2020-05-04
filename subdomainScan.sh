9f34bb135edb94297334ea9a128cc99304300815
neo4277 <11756419+neo4277@users.noreply.github.com>
2020-05-04 03:10:21 -0500

    Add files via upload

--- subdomainScan.sh
+++ subdomainScan.sh
@@ -17,8 +17,8 @@ mkdir -p ${folder}
 echo Checking shodan...
 shodan domain $domain | sed "s,$(printf '\033')\\[[0-9;]*[a-zA-Z],,g" > "${folder}""shodan ${time}"
 
-echo Shodan CNAME only...
-echo "${folder}""shodan ${time}" | grep CNAME > echo "${folder}""shodan CNAME only ${time}"
+#echo Shodan CNAME only...
+#echo "${folder}""shodan ${time}" | grep CNAME > echo "${folder}""shodan CNAME only ${time}"
 
 echo Checking sublist3r...
 python /Users/$USER/Sublist3r/sublist3r.py -d $domain -n -o "${folder}""sublist3r ${time}" > /dev/null
@@ -31,6 +31,36 @@ for file in "${folder}sublist3r ${time}"; do
 done
 echo "$(<"${folder}sublist3r DiG Records ${time}")" | grep CNAME > "${folder}""sublist3r with CNAME ${time}"
 
-#http -b GET http://{SOURCE DOMAIN NAME} | grep -E -q '<Code>NoSuchBucket</Code>|<li>Code: NoSuchBucket</li>' && echo "Subdomain takeover may be possible" || echo "Subdomain takeover is not possible"
+echo Processing data...
+awk -v RS=" " '{print}' "${folder}sublist3r with CNAME ${time}" > "${folder}sublist3r CNAME spaces"
+awk -v RS="	" '{print}' "${folder}sublist3r CNAME spaces" > "${folder}sublist3r CNAME spaces 2"
+#awk -v RS=" " '{print}' "${folder}sublist3r CNAME spaces" > "${folder}sublist3r CNAME spaces"
+#awk -v RS="	" '{print}' "${folder}sublist3r CNAME spaces" > "${folder}sublist3r CNAME spaces"
+
+aws="amazonaws"
+github="github.io"
+heroku="herokudns.com"
+readme="readme.io"
+
+echo Checking scans for possible subdomain takeovers...
+echo "$(<"${folder}sublist3r CNAME spaces 2")" | grep -e amazonaws -e github.io -e herokudns -e readme.io > "${folder}sublist3r CNAME spaces 3"
+while read -r line
+do
+	echo --------------------------
+	echo "$line"
+	echo --------------------------
+	
+	echo Detecting Amazon...
+	grep -q 'amazonaws' <<< $line && (http --ignore-stdin -b GET http://$line | grep -E -q '<Code>NoSuchBucket</Code>|<li>Code: NoSuchBucket</li>' && echo "[!!!] $line - Subdomain takeover may be possible for $line" || echo X)
+
+	echo Detecting Github...
+	grep -q 'github.io' <<< $line && (http --ignore-stdin -b GET http://$line | grep -F -q "<strong>There isn't a GitHub Pages site here.</strong>" && echo "[!!!] $line - Subdomain takeover may be possible for $line" || echo X)
+
+	echo Detecting Heroku...
+	grep -q 'herokudns.com' <<< $line && (http --ignore-stdin -b GET http://$line | grep -F -q "//www.herokucdn.com/error-pages/no-such-app.html" && echo "[!!!] $line - Subdomain takeover may be possible for $line" || echo X)
+
+	echo Detecting Readme...
+	grep -q 'readme.io' <<< $line && (http --ignore-stdin -b GET http://$line | grep -F -q "Project doesnt exist... yet!" && echo "[!!!] $line - Subdomain takeover may be possible" || echo X)
+done < "${folder}sublist3r CNAME spaces 3"
 
 echo Done!
